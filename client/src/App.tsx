b73646c185579d3454701ace9a5052ad23b8a6dd
Pedro Ferreira <pedroh@fetch.ly>
2022-06-26 22:40:52 -0300

    Socket io chat for study

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
+
 import "./App.css";
 import { socket } from "./utils/socket/socket";
 import { displayMessage } from "./utils/displayMessages";
+import { MdSend } from "react-icons/md";
 import Messages from "./utils/socket/messages";
 import AuthService from "./utils/socket/connection";
 
 function App() {
-  const messageRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
   const roomRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -19,12 +21,27 @@ function App() {
   }, []);
 
   const sendMessage = (message: string, room?: string) => {
-    if (room) {
-      Messages.sendMessage(message, room);
-    } else {
-      Messages.sendMessage(message);
+    if (message) {
+      if (room) {
+        Messages.sendMessage(message, room);
+      } else {
+        Messages.sendMessage(message);
+      }
+      if (messageRef.current) {
+        messageRef.current.value = "";
+        displayMessage(message, "sent");
+      }
     }
-    displayMessage(message, "sent");
+  };
+
+  const onEnterPressed = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      sendMessage(
+        messageRef.current?.value || "",
+        roomRef.current?.value || ""
+      );
+    }
   };
 
   const joinRoom = (room: string) => {
@@ -33,22 +50,34 @@ function App() {
 
   return (
     <div className="App">
-      <div className="window-messages"></div>
-      <input ref={messageRef} />
-      <button
-        onClick={() =>
-          sendMessage(
-            messageRef.current?.value || "",
-            roomRef.current?.value || ""
-          )
-        }
-      >
-        Send
-      </button>
-      <input ref={roomRef} />
-      <button onClick={() => joinRoom(roomRef.current?.value || "")}>
-        Send
-      </button>
+      <div className="window-messages" />
+      <div className="join-group-container">
+        <input placeholder="Type a group to join" ref={roomRef} />
+        <button onClick={() => joinRoom(roomRef.current?.value || "")}>
+          Join
+        </button>
+      </div>
+      <div className="textarea-container">
+        <textarea
+          ref={messageRef}
+          onKeyDown={onEnterPressed}
+          id="chatText"
+          className="chat-text"
+          placeholder="Send a message"
+        />
+        <div
+          className="send-button"
+          id="sendButton"
+          onClick={() =>
+            sendMessage(
+              messageRef.current?.value || "",
+              roomRef.current?.value || ""
+            )
+          }
+        >
+          <MdSend />
+        </div>
+      </div>
     </div>
   );
 }
